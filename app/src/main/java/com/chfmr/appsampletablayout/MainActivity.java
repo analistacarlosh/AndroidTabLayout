dd4c7a2152a761321323ab88ee27d7af6baba395
Carlos Rodrigues <analista.carlosh@gmail.com>
2016-02-16 13:02:31 -0200

    [TabLayout] - Implements Basic Functional TabLayout

--- app/src/main/java/com/chfmr/appsampletablayout/MainActivity.java
+++ app/src/main/java/com/chfmr/appsampletablayout/MainActivity.java
@@ -1,9 +1,18 @@
 package com.chfmr.appsampletablayout;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it' PagerAdapter
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        //viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+        if(viewPager != null){
+            setupViewPager(viewPager);
+        }
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new BlankFragmentOne(), "Category 1");
+        adapter.addFragment(new BlankFragmentTwo(), "Category 2");
+        adapter.addFragment(new BlankFragmentThree(), "Category 3");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
